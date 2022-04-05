0b2ed0e405a80b320e241d2d88f1551520c1360b
SerhiiPetrenk0 <suation196@gmail.com>
2022-04-05 12:53:08 +0300

    fix duplicate components folders

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,9 @@
 import { ListProducts } from './ListProducts';
 import { ItemProducts } from './ItemProducts';
 //Components
-import { Comment } from './components/Comment';
-import { PostForm } from './components/PostForm';
-import { StarBar } from './components/StarBar';
+import { Comment } from './other/Comment';
+import { PostForm } from './other/PostForm';
+import { StarBar } from './other/StarBar';
 
 export { 
     ListProducts,
