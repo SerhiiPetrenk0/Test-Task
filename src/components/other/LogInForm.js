36f1ea86345949c5283fbdfa086960915d17b2cd
SerhiiPetrenk0 <suation196@gmail.com>
2022-04-11 12:35:29 +0300

    + Title registration

--- src/components/other/LogInForm.js
+++ src/components/other/LogInForm.js
@@ -28,7 +28,7 @@ export const LogInForm = props => {
       <>
         <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
+            <Modal.Title>Registration</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form onSubmit={formik.handleSubmit}>
